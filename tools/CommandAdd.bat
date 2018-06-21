1c342a8c56454a525ee3319656c453f87df7522d
68681395 <happycoder@foxmail.com>
2018-06-21 10:57:47 +0800

    simplify structure of folders

--- tools/CommandAdd.bat
+++ tools/CommandAdd.bat
@@ -7,20 +7,20 @@ set name=Proxy Disable
 
 reg add "HKEY_CLASSES_ROOT\Drive\shell\%key%" /ve /d "%name%" /f
 reg add "HKEY_CLASSES_ROOT\Drive\shell\%key%" /v "NoWorkingDirectory" /d "" /f
-reg add "HKEY_CLASSES_ROOT\Drive\shell\%key%\command" /ve /d "%~sdp0runner.exe \"%~sdp0%key%.bat\"" /f
+reg add "HKEY_CLASSES_ROOT\Drive\shell\%key%\command" /ve /d "%~sdp0runas.exe \"%~sdp0%key%.bat\"" /f
 
 reg add "HKEY_CLASSES_ROOT\Directory\shell\%key%" /ve /d "%name%" /f
 reg add "HKEY_CLASSES_ROOT\Directory\shell\%key%" /v "NoWorkingDirectory" /d "" /f
-reg add "HKEY_CLASSES_ROOT\Directory\shell\%key%\command" /ve /d "%~sdp0runner.exe \"%~sdp0%key%.bat\"" /f
+reg add "HKEY_CLASSES_ROOT\Directory\shell\%key%\command" /ve /d "%~sdp0runas.exe \"%~sdp0%key%.bat\"" /f
 
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\%key%" /ve /d "%name%" /f
 reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\%key%" /v "NoWorkingDirectory" /d "" /f
-reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\%key%\command" /ve /d "%~sdp0runner.exe \"%~sdp0%key%.bat\"" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\%key%\command" /ve /d "%~sdp0runas.exe \"%~sdp0%key%.bat\"" /f
 
 reg add "HKEY_CLASSES_ROOT\LibraryFolder\shell\%key%" /ve /d "%name%" /f
 reg add "HKEY_CLASSES_ROOT\LibraryFolder\shell\%key%" /v "NoWorkingDirectory" /d "" /f
-reg add "HKEY_CLASSES_ROOT\LibraryFolder\shell\%key%\command" /ve /d "%~sdp0runner.exe \"%~sdp0%key%.bat\"" /f
+reg add "HKEY_CLASSES_ROOT\LibraryFolder\shell\%key%\command" /ve /d "%~sdp0runas.exe \"%~sdp0%key%.bat\"" /f
 
 reg add "HKEY_CLASSES_ROOT\LibraryFolder\Background\shell\%key%" /ve /d "%name%" /f
 reg add "HKEY_CLASSES_ROOT\LibraryFolder\Background\shell\%key%" /v "NoWorkingDirectory" /d "" /f
-reg add "HKEY_CLASSES_ROOT\LibraryFolder\Background\shell\%key%\command" /ve /d "%~sdp0runner.exe \"%~sdp0%key%.bat\"" /f
+reg add "HKEY_CLASSES_ROOT\LibraryFolder\Background\shell\%key%\command" /ve /d "%~sdp0runas.exe \"%~sdp0%key%.bat\"" /f
